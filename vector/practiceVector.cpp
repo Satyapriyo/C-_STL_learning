33510b5b88dfe941af7e05255e603d781375fe15
Satyapriyo Biswas <satyapriyoak47@gmail.com>
2022-08-16 01:00:00 +0530

    updating vector

---
+++ vector/practiceVector.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> a ;
+    a.push_back(1);
+    a.push_back(2);
+    a.push_back(3);
+    a.push_back(4);
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout<<a[i]<<' ';
+    }
+    return 0;
+}
